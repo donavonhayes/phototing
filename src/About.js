09f28c0b0a985a06fb30f006282e2968e5af4ae6
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2024-05-08 14:43:39 -0500

    8 commit

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '20px' }}>
     <div style={{ flex: '1', textAlign: 'right', marginRight: '50px' }}>
-        <img src="https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/406142916_10221324391133680_718012415532186999_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=3635dc&_nc_ohc=cgW6QHTVyVkAX-ozCNw&_nc_ht=scontent-ord5-1.xx&oh=00_AfAE0GfLbq4xh1-RZR1saPo5CjNxKcrrKqu3XJCjAMKXcQ&oe=65AC84B1" alt="About Us" style={{ width: '300px', height: '300px', }} />
+        <img src="https://scontent-ord5-1.xx.fbcdn.net/v/t39.30808-6/441383027_10221970055514886_1578824059436832814_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=fbOLFmQBFUYQ7kNvgH7ZNuk&_nc_ht=scontent-ord5-1.xx&oh=00_AfCa5wxJvu9ehKvXmMV-kLIBUdu7ppL3z1gnRzms6JQKfQ&oe=6641ABB6" alt="About Us" style={{ width: '300px', height: '300px', }} />
       </div>
       <div>
         <h2>About Shutter Fields Photography</h2>
